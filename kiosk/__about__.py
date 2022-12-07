d4d89acde6f1786a9f7e792ef4fb34969f49029f
larnaa <juliakorni1997@yandex.ru>
2022-12-07 17:16:38 -0300

    rename kiosk to src

--- kiosk/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present larnaa <juliakorni1997@yandex.ru>
-#
-# SPDX-License-Identifier: MIT
-__version__ = '0.0.1'
--- kiosk/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present larnaa <juliakorni1997@yandex.ru>
-#
-# SPDX-License-Identifier: MIT
