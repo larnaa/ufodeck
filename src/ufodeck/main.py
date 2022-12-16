d7bc9436d9217c76f8b32bc7d268c8ba5cc7a74b
larnaa <juliakorni1997@yandex.ru>
2022-12-16 13:27:49 -0300

    add subprocess turn off windows

--- src/ufodeck/main.py
+++ src/ufodeck/main.py
@@ -10,5 +10,13 @@ def switch_on_windows(switch: str) -> str:
             stderr=subprocess.PIPE,
             check=False,
         )
+    if switch ==  "off":
+        result = subprocess.run(
+            ["echo", "Windows closing..."],
+            shell=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=False,
+        )
 
     return result.stdout.decode()
