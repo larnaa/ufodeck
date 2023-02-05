dc218119d7a435595217a57277c7b433f88cd3f1
Lev Gorodetskiy <github@droserasprout.space>
2023-02-05 14:47:58 -0300

    More sleeps

--- src/ufodeck/scripts/gpu_nvidia.sh
+++ src/ufodeck/scripts/gpu_nvidia.sh
@@ -3,8 +3,8 @@
 echo "=> Unloading vfio-pci kernel module"
 modprobe -r vfio_pci
 
-echo "=> Waiting 5 seconds"
-sleep 5
+echo "=> Waiting 2 seconds"
+sleep 2
 
 echo "=> Attaching GPU"
 virsh nodedev-reattach pci_0000_0b_00_1
@@ -13,14 +13,17 @@ virsh nodedev-reattach pci_0000_0b_00_0
 echo "=> Attaching wlp4s0"
 virsh nodedev-reattach pci_0000_04_00_0
 
+echo "=> Waiting 2 seconds"
+sleep 2
+
 echo "=> Loading nvidia kernel modules"
 modprobe nvidia
 modprobe nvidia_modeset
 modprobe nvidia_uvm
 modprobe nvidia_drm
 
-echo "=> Waiting 5 seconds"
-sleep 5
+echo "=> Waiting 2 seconds"
+sleep 2
 
 echo "=> Binding efi-framebuffer"
 echo efi-framebuffer.0 | tee /sys/bus/platform/drivers/efi-framebuffer/bind
@@ -31,3 +34,9 @@ echo 0000:04:00.0 | tee /sys/bus/pci/drivers/iwlwifi/bind
 echo "=> Binding VTs"
 echo 1 | tee /sys/class/vtconsole/vtcon0/bind
 echo 1 | tee /sys/class/vtconsole/vtcon1/bind
+
+echo "=> Starting DM"
+systemctl start gdm.service
+
+echo "=> Waiting 2 seconds"
+sleep 2
--- src/ufodeck/scripts/gpu_vfio.sh
+++ src/ufodeck/scripts/gpu_vfio.sh
@@ -3,14 +3,17 @@
 echo "=> Stopping DM"
 systemctl stop gdm.service
 
+echo "=> Waiting 2 seconds"
+sleep 2
+
 echo "=> Unbinding iwlwifi"
 echo 0000:04:00.0 | tee /sys/bus/pci/drivers/iwlwifi/unbind
 
 echo "=> Unbinding efi-framebuffer"
 echo efi-framebuffer.0 | tee /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-echo "=> Waiting 5 seconds"
-sleep 5
+echo "=> Waiting 2 seconds"
+sleep 2
 
 echo "=> Unloading nvidia kernel modules"
 modprobe -r nvidia_drm
@@ -18,8 +21,8 @@ modprobe -r nvidia_uvm
 modprobe -r nvidia_modeset
 modprobe -r nvidia
 
-echo "=> Waiting 5 seconds"
-sleep 5
+echo "=> Waiting 2 seconds"
+sleep 2
 
 echo "=> Detaching GPU"
 virsh nodedev-detach pci_0000_0b_00_0
@@ -30,3 +33,6 @@ virsh nodedev-detach pci_0000_04_00_0
 
 echo "=> Loading vfio-pci kernel module"
 modprobe vfio-pci
+
+echo "=> Waiting 2 seconds"
+sleep 2
